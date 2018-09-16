04376454dc5f1b636ef632da86f3c547d0547151
Timothy Hoang <timh1203@gmail.com>
2018-09-16 10:38:56 -0400

    Add redux-thunk

--- src/index.js
+++ src/index.js
@@ -6,16 +6,24 @@ import registerServiceWorker from './registerServiceWorker';
 
 // 1. Import redux
 // 8. Import Provider
-import { combineReducers, createStore } from 'redux'
+// 12. Import redux-thunk
+import { applyMiddleware, compose, combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import productsReducer from './reducers/productsReducer'
 import userReducer from './reducers/userReducer'
+import thunk from 'redux-thunk'
 
 // 5. Combine all the reducers together
 const allReducers = combineReducers({
   products: productsReducer,
   user: userReducer
 })
+
+// 13. Adding redux-thunk
+const allStoreEnhancers = compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension && window.devToolsExtension()
+)
 
 // 2. Create the store with combineReducers
 // 6. Adding an initial state
@@ -28,7 +36,7 @@ const store = createStore(
     }],
     user: 'Michael'
   },
-  window.devToolsExtension && window.devToolsExtension()
+  allStoreEnhancers
 )
 
 // 3. Create the root reducer and subreducers
