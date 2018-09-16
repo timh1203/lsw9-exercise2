e5a82536dd4f41e63c538783e8b81ec85b93e02a
Timothy Hoang <timh1203@gmail.com>
2018-09-16 09:25:24 -0400

    Add basic flow example with action

--- src/index.js
+++ src/index.js
@@ -4,5 +4,29 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// 1. Create the store
+import { createStore } from 'redux'
+const store = createStore(reducer)
+
+// 2. Create the root reducer
+function reducer(state, action) {
+  if (action.type === 'CHANGE_STATE') {
+    return action.payload.newState
+  }
+  
+  return 'State'
+}
+
+// 3. Create an action
+const action = {
+  type: "CHANGE_STATE",
+  payload: {
+    newState: 'Change the state'
+  }
+}
+
+store.dispatch(action)
+console.log(store.getState())
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
